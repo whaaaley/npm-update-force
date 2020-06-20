e15508d94f544ad28fa93a96fbf7bdc78d5cf588
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2020-06-20 02:26:43 -0500

    commit project

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# npm-update-force
+
+Updates every package in package.json to it's latest version
+
+## Install
+
+```
+$ npm i -g @whaaaley/npm-update-force
+```
+
+## Usage
+
+```
+$ cd your-project-here
+$ npm-update-force
+```
+
+## Preview
+
+![preview](preview.png)
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const child_process = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const color = require('./color')
+
+const data = JSON.parse(fs.readFileSync(path.resolve('package.json')))
+
+const deps = {
+  'npm i': data.dependencies,
+  '; npm i -D': data.devDependencies,
+  '; npm i -O': data.optionalDependencies
+}
+
+let cmd = ''
+
+for (const key in deps) {
+  const group = deps[key]
+
+  cmd += key
+
+  if (group) {
+    for (const name in group) {
+      cmd += ' ' + name + '@latest'
+    }
+  }
+}
+
+console.log(
+  color('blue', '\n[npm-update-force]'),
+  color('green', 'Updating...'),
+  color('white', cmd + '\n')
+)
+
+child_process.exec(cmd, { maxBuffer: Infinity }, () => {
+  console.log(
+    color('blue', '\n[npm-update-force]'),
+    color('green', 'Done!\n')
+  )
+}).stdout.pipe(process.stdout)
